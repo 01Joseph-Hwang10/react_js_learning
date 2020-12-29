416b2c42d584fb37cc27da6ad39e07f9c1a9a27d
01Joseph-Hwang10 <72839543+01Joseph-Hwang10@users.noreply.github.com>
2020-12-29 15:13:10 +0900

    react fundamentals finished

--- movie_app_2019/src/App.js
+++ movie_app_2019/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import Potato from "./Potato";
+// import Potato from "./Potato";
+import axios from "axios";
+import Movie from './Movie';
+import "./App.css"
 
 class App extends React.Component{
 
@@ -7,19 +10,43 @@ class App extends React.Component{
     isLoading:true,
     movies:[]
   };
+  getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies)
+    this.setState({ movies, isLoading:false })
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading:false,book:true})
-    },6000)
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>
-      {isLoading ? "Loading..." : "We are ready"}
-      {/* js ternary operator : if true else */}
-      </div> 
+    const { isLoading, movies } = this.state;
+    return (<section className="container">
+      {isLoading ? (
+        <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+      ) : (
+        <div className="movies">
+          {
+            movies.map(movie => {
+              return <Movie 
+              key={movie.id} 
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+               />
+            })
+          }
+        </div>
+      )
+    }
+      </section> )
   }
 }
 
+// js ternary operator : if true else
 
 export default App;
